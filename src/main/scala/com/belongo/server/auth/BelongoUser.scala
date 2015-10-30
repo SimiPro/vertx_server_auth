1599d574d6d447dc921d3135cbb1d8f4cdca64dd
simipro <sihuber@gmx.net>
2015-10-30 19:11:08 +0100

    add auth test

---
+++ src/main/scala/com/belongo/server/auth/BelongoUser.scala
@@ -0,0 +1,13 @@
+package com.belongo.server.auth
+
+import com.fasterxml.jackson.databind.BeanProperty
+
+/**
+ * Created by simipro on 30/10/15.
+ */
+class BelongoUser( username:String,  email:String) {
+
+
+
+
+}
